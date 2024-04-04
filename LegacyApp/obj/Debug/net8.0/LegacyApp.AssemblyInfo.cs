12b823f14c680e37a97406e4d26119793520420e
s27168 <s27168@pjwstk.edu.pl>
2024-04-04 17:28:20 +0200

    Commit

--- LegacyApp/obj/Debug/net8.0/LegacyApp.AssemblyInfo.cs
+++ LegacyApp/obj/Debug/net8.0/LegacyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LegacyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab2989597cc62e4e3392af1a7ed1f74e052d0a41")]
 [assembly: System.Reflection.AssemblyProductAttribute("LegacyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LegacyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
